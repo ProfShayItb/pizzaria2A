dc701a3e7a0f9b8f539839f3450c928e88aa2efd
Sharlayne Angel Jardini Fonseca <sharlayne.fonseca@docente.fieb.edu.br>
2024-11-13 18:58:03 -0300

    Código atualizado

--- src/pages/Catalogo/index.jsx
+++ src/pages/Catalogo/index.jsx
@@ -33,14 +33,25 @@ export default function Catalogo() {
 
 
             <div className='row'>
-                {produto.map((item)=>{
+                {produto.map((item)=>(
                     <div className='col-md-4 mb-4' key={item.id}>
                         <div className='card h-100'>
-                            <img src={item.urlFoto}></img>
 
+                            <div className='card-img-top' alt={item.nome}>
+                                
+                            <img src={item.urlFoto}></img>{/*supondo que a imagem tenha uma url*/}
+                            </div>
+                            <div className='card-body'>
+                                <h5 className='card-title'>{item.nome}</h5>
+                                <p className='card-text'>{item.descricao}</p>
+                                <p className='card-text'><strong>Preço: </strong> R$ {item.preco.toFixed(2)}</p>
+                            </div>
+                            <div className="card-footer text-center">
+                                <button className="btn btn-primary">Comprar</button>
+                            </div>
                             </div>
                         </div>
-                })}
+                ))}
             </div>
 
 
